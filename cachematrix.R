6ee59fb29bc59df69b5b3c41b4d2ab1c23324683
sergio8614 <sergio.rondon@outlook.com>
2016-07-03 20:07:35 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,12 @@
-## Below are two functions thar are used to create a special
-## object that stores a square matrix and caches 
-## its inverse matrix
+## Below are two functions that are used to create a special
+## object that stores a square matrix and caches its inverse matrix
 
 ## makeCacheMatrix creates a special "matrix", which is really
 ## containing a function to:
-##		1- set the value of the matrix
-##		2- get the value of the matrix
-##		3- set the value of the inverse matrix
-##		4- get the value of the inverse matrix
+##	1- set the value of the matrix
+##	2- get the value of the matrix
+##	3- set the value of the inverse matrix
+##	4- get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
